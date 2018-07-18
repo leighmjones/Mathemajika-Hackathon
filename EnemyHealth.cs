19cceba503da693fc2014f062e5a490d6537dedc
Leigh Jones <40029484+leighmjones@users.noreply.github.com>
2018-07-18 13:10:31 -0500

    Add files via upload

---
+++ EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+
+
+    public float Health = 100;
+
+    public void TakeHits (float damage)
+    {
+        Health -= damage;
+        if (Health = 0)
+        {
+            Destroy(GameObject/*?*/);
+        }
+    }
+    
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
